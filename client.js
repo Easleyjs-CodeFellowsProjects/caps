6efc4761669058c87a09fdbec580d827a100134e
Josh Easley <easleyjs@gmail.com>
2023-10-05 21:01:57 -0700

    Adds second Vendor to end-to-end test

--- client.js
+++ client.js
@@ -10,10 +10,16 @@ const STORE_NAME2 = 'acme-widgets';
 
 const myDriver = new Driver( CONNECTION_STRING );
 const myVendor = new Vendor( CONNECTION_STRING, STORE_NAME );
+const mySecondVendor = new Vendor( CONNECTION_STRING, STORE_NAME2 );
 
 const orderDetails = {
     customer: 'Roy Thomas',
     address: '1234 Main St., Omaha, NE 92630'
+}
+
+const orderSecondDetails = {
+    customer: 'Jimmy McElroy',
+    address: '628 Scofield Ln, Pittsburgh, PA 56232'
 }
 
 console.log('Initializing Driver');
@@ -24,6 +30,12 @@ console.log('Initializing Vendor');
 myVendor.connect();
 myVendor.listenForDelivery();
 
+console.log('Initializing Second Vendor');
+mySecondVendor.connect();
+mySecondVendor.listenForDelivery();
 
 console.log('Creating Delivery');
 myVendor.createPickupEmit(orderDetails);
+
+console.log('Creating Second Delivery');
+mySecondVendor.createPickupEmit(orderSecondDetails);
